a9f34b2346283bea4323cef5f3c95e3468aa3147
lillianlayne <lillianlayne22@gmail.com>
2024-04-02 10:38:03 -0400

    OAuth and Passport setup

--- server.js
+++ server.js
@@ -3,9 +3,12 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const session = require('express-session');
+const passport = require('passport');
 
 require('dotenv').config();
 require('./config/database');
+require('./config/passport');
 
 const livereload = require('livereload');
 const connectLiveReload = require('connect-livereload');
@@ -37,6 +40,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false, 
+  saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/drinks', drinksRouter);
