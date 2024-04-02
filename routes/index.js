0053e4a6a6c93206e8fd1a27e90400f752027d8f
lillianlayne <lillianlayne22@gmail.com>
2024-04-02 10:53:47 -0400

    passport and google oauth route setup

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
-var express = require('express');
-var router = express.Router();
-
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
 const indexCtrl = require('../controller/indexController');
 
 // all paths relative to index
@@ -9,6 +9,27 @@ const indexCtrl = require('../controller/indexController');
 router.get('/', indexCtrl.index)
 // GET /categories/drinks
 router.get('/categories/drinks/:category', indexCtrl.showCategory)
+// GET Google OAuth login route
+router.get('/auth/google', passport.authenticate(
+    'google', 
+    {
+        scope: ['profile', 'email']
+    }
+));
+// GET Google OAuth callback route
+router.get('/oauth2callback', passport.authenticate(
+    'google', 
+    {
+        successRedirect: '/',
+        failureRedirect: '/'
+    }
+));
+// GET logout route
+router.get('/logout', function(req, res) {
+    req.logout(function() {
+        res.redirect('/');
+    });
+})
 
 
 module.exports = router;
