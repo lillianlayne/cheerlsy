d943d7f3e01eb0814654bbf07c667a4285623af3
lillianlayne <lillianlayne22@gmail.com>
2024-04-03 10:50:21 -0400

    state abbreviation input, parse lowercase data

--- controller/reviewsController.js
+++ controller/reviewsController.js
@@ -9,10 +9,20 @@ const renderForm = async (req, res) => {
 }
 
 const createReview = async (req, res) => {
+    req.body.user = req.user._id;
+    req.body.userName = req.user.name;
+    req.body.userIcon = req.user.avatar
+      
+    // save inputs as lowercase
+    req.body.restaurant = req.body.restaurant.toLowerCase();
+    req.body.city = req.body.city.toLowerCase();
+    req.body.state = req.body.state.toLowerCase();
+
     const review = await Reviews.create(req.body);
     const drink = await Drinks.findById(req.params.id);
-
+    
     drink.reviews.push(review)
+    
     try {
         await drink.save();    
     } catch (error) {
