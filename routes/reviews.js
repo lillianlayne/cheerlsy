c375618b59b7b8d45caa34fc344544cc5025ffe1
lillianlayne <lillianlayne22@gmail.com>
2024-04-05 11:17:33 -0400

    ensure userloggedin

--- routes/reviews.js
+++ routes/reviews.js
@@ -7,9 +7,9 @@ const ensureLoggedIn = require('../config/ensureLoggedIn');
 
 // all paths relative to /drinks
 // GET /form/:id - render form to add a review for a certain drink
-router.get('/drinks/form/:id', reviewsCtrl.renderForm);
+router.get('/drinks/form/:id',ensureLoggedIn, reviewsCtrl.renderForm);
 // post review for drink 
-router.post('/drinks/reviews/:id', reviewsCtrl.create);
+router.post('/drinks/reviews/:id', ensureLoggedIn, reviewsCtrl.create);
 
 module.exports = router;
 
